68e9c35c85b01ff555877e3a965258c8b3145b70
Gizem Yumun <giyumun@k1m11s02.42kocaeli.com.tr>
2023-02-24 17:05:15 +0300

    seda

--- repeat_alpha/repeat_alpha.c
+++ repeat_alpha/repeat_alpha.c
@@ -12,6 +12,8 @@ int	main(int ac, char **av)
 				t += av[1][i] - 96;
 			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
 				t += av[1][i] - 64;
+			else
+				t = 1;
 			while (t)
 			{
 				write(1, &av[1][i], 1);
